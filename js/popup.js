fff3ad69474ea943933ecd9327b222417e3c3ac8
Glean <96066463+ImGlean@users.noreply.github.com>
2023-11-04 20:15:20 -0400

    General improvements
    Added loading skeletons
    Increased PP value accuracy
    Made accuracy slider updates in real-time

--- js/popup.js
+++ js/popup.js
@@ -6,15 +6,23 @@ chrome.tabs.query({
   if (tab.url.substring(0,31) == "https://osu.ppy.sh/beatmapsets/" && tab.url.length > 30){
     const beatmapStringArray = tab.url.substring(31).split("#");
     const beatmapSetID = beatmapStringArray[0];
-    const osuModesArray = ["osu", "taiko", "fruits", "mania"];
-    const osuMode = osuModesArray.indexOf(beatmapStringArray[1].split("/")[0]);
     const difficultyID = beatmapStringArray[1].split("/")[1];
-    const apiURLAdditions = "&s=" + beatmapSetID + "&b=" + difficultyID + "&m=" + osuMode; 
-    const beatmapCoverImg = "https://assets.ppy.sh/beatmaps/" + beatmapSetID + "/covers/cover.jpg";
     const ppcalcAPIURL = "https://pp.osuck.net/pp?id=" + difficultyID;
+    const slider = document.getElementById("accSlider");
 
     document.getElementById("navigationError").hidden = true;
-
+    document.getElementById("coverImage").src = "https://assets.ppy.sh/beatmaps/" + beatmapSetID + "/covers/cover.jpg";
+    document.getElementById("rankAchieved").src = "https://osu.ppy.sh/assets/images/GradeSmall-S.3b4498a9.svg";
+    document.getElementById("accSlider").addEventListener("input", (event) => {
+      document.getElementById("accSliderLabel").textContent = "Accuracy: " + event.target.value + "%";
+    });
+    slider.style.background = 'rgb(255,204,33)';
+    slider.addEventListener("input", function(){
+      let x = (slider.value * 5) - 400;
+      let color = 'linear-gradient(90deg, rgb(255,204,33) ' + x + '%, rgb(17,24,28) ' + x + '%)';
+      slider.style.background = color;
+    })
+    
     function recalcPP() {
       let modID = 0;
   
@@ -43,11 +51,16 @@ chrome.tabs.query({
         } 
       }) 
       .then(data => {
-        console.log(ppcalcAPIURL + "&mods=" + modID); // Testing
+        console.log(ppcalcAPIURL + "&mods=" + modID); 
         let accuracy = Math.round(document.getElementById("accSlider").value * 10) / 10;
         let performancePoints = data.pp.acc[Math.round(accuracy)];
         if (Math.round(accuracy) != accuracy) { // If accuracy is not int
-          performancePoints = (data.pp.acc[Math.floor(accuracy)] + data.pp.acc[Math.ceil(accuracy)]) / 2; // Approximate non int acc pp values
+          // Attempt to fix accuracy curve
+          if (accuracy > 97){ 
+            performancePoints = Math.round(((data.pp.acc[Math.floor(accuracy)] * 0.6) + (data.pp.acc[Math.ceil(accuracy)] * 0.4)) * 10) / 10;
+          } else {
+            performancePoints = (data.pp.acc[Math.floor(accuracy)] + data.pp.acc[Math.ceil(accuracy)]) / 2;
+          }
         } else { // If accuracy is int
           performancePoints = data.pp.acc[accuracy];
         }
@@ -55,7 +68,6 @@ chrome.tabs.query({
         document.getElementById("mapStats").innerHTML = "AR: " + data.stats.ar + " OD: " + data.stats.od + " CS: " + data.stats.cs + " HP: " + data.stats.hp + " BPM: " + data.stats.bpm.api; 
         document.getElementById("stars").innerHTML = data.stats.star.pure;
         document.getElementById("performancePoints").innerHTML = performancePoints + "pp <span id='offWhite'>for " + accuracy + "%<span/>";
-        document.getElementById("accSliderLabel").innerHTML = "Accuracy: " + accuracy + "%";
         
         if (accuracy > 85 && accuracy <= 91) {
           document.getElementById("rankAchieved").src="https://osu.ppy.sh/assets/images/GradeSmall-A.d785e824.svg";
@@ -98,7 +110,14 @@ chrome.tabs.query({
           data.data.title = (data.data.title).substring(0,23) + "...";
         }
 
-        document.getElementById("coverImage").src = beatmapCoverImg;
+        if ((data.data.diff).length >= 24) {
+          data.data.diff = (data.data.diff).substring(0,21) + "...";
+        }
+
+        if ((data.data.artist).length >= 32) {
+          data.data.artist = (data.data.artist).substring(0,29) + "...";
+        }
+
         document.getElementById("beatmapDifficultyName").innerHTML = "[" + data.data.diff + "]";
         document.getElementById("beatmapName").innerHTML = data.data.title;
         document.getElementById("artistName").innerHTML = data.data.artist;
@@ -126,6 +145,9 @@ chrome.tabs.query({
     document.getElementById("extensionDetails").hidden = true;
     document.getElementById("coverImageCrop").style.height = "1px";
     document.getElementById("coverImageCrop").style.paddingBottom = "0px";
+    document.getElementById("coverImage").src = "";
+    document.getElementById("beatmapDetails").innerHTML = "";
+    document.getElementById("starRatingDiv").innerHTML = "";
     console.log("Not on an osu! Beatmap Page");
   }
 
